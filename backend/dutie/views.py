8ac50bad3bcf773aa732830c4fe48327a9f9024a
oleg_zubkov <zoleg1030@gmail.com>
2024-02-18 13:37:42 +0300

    create viewsets

--- backend/dutie/views.py
+++ backend/dutie/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = Todo
+    queryset = Todo.objects.all()
+
