4a540acbe127fa73359695975f6896be1d6a9554
oleg_zubkov <zoleg1030@gmail.com>
2024-02-18 13:23:50 +0300

    settings and new model

--- backend/dutie/models.py
+++ backend/dutie/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
