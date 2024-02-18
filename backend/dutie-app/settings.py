b6560e69615bd3381d6560b0a26426e850462c39
oleg_zubkov <zoleg1030@gmail.com>
2024-02-18 13:35:38 +0300

    install rest and create serializer

--- backend/dutie-app/settings.py
+++ backend/dutie-app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'dutie',
+    'corsheaders',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Whitelisting React port
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000'
+)
